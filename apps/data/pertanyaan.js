74ebd0cfd5ecedea95559fab0226298f8bc0c3bf
Zen <muhzaini30@gmail.com>
2022-07-22 12:03:09 +0800

    simpan

---
+++ apps/data/pertanyaan.js
@@ -0,0 +1,10 @@
+export default [
+  "Apakah kredit itu harus memegang barangnya terlebih dahulu?",
+  "Apakah nyicil barang itu termasuk riba?",
+  "Apakah pelaku riba syariah itu keluar dari agama?",
+  "Apakah barang gadai boleh dimanfaatkan?",
+  "Apakah lebih baik tidak menabung di bank?",
+  "Apakah melunasi utang di bank termasuk riba?",
+  "Bolehkah sedekah menggunakan uang riba?",
+  "Kalau kita misalnya di hutan, terus kita kelaparan, terus makanan yang kita bawa habis, terus di situ hanya ada babi, terus kalau kita nggak makan, kita akan mati, nah terus apakah boleh kita makan babi itu?",
+];
