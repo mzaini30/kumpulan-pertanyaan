117405643f1eca5e95684cf7aefcf84e0699adff
Zen <muhzaini30@gmail.com>
2022-07-23 14:36:10 +0800

    upload

--- apps/data/pertanyaan.js
+++ apps/data/pertanyaan.js
@@ -7,4 +7,8 @@ const pertanyaan = [
   "Apakah melunasi utang di bank termasuk riba?",
   "Bolehkah sedekah menggunakan uang riba?",
   "Kalau kita misalnya di hutan, terus kita kelaparan, terus makanan yang kita bawa habis, terus di situ hanya ada babi, terus kalau kita nggak makan, kita akan mati, nah terus apakah boleh kita makan babi itu?",
+  "Apakah boleh memasang foto anime di kamar?",
+  "Melihat aurat di kartun apakah zina mata?",
+  "Buku Mukadiman Ibnu Khaldun ditulis dalam bahasa apa?",
+  "Apa yang dimaksud dengan kesiapan diri sebelum belajar?",
 ];
